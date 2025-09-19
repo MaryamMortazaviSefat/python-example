57d0816cc5a5ea191b86c100af71025e7bccd4fd
MarayamMortazaviSefat <marammortazavi@gmail.com>
2025-09-19 09:51:21 +0330

    -

--- work_with_csv/work_with_csv.py
+++ work_with_csv/work_with_csv.py
@@ -1,5 +1,26 @@
 import csv
+import os
 
-with open("products.csv","r", encoding="utf-8") as file:
+script_dir=os.path.dirname(__file__)
+file_path=os.path.join(script_dir,"products.csv")
+
+with open(file_path,"r") as file:
     reader=csv.DictReader(file)
     products=list(reader)
+
+print(products)
+
+new_data=[]
+for product in products:
+    name=product["Product Name"]
+    price=int(product["Price"])
+    quantity=int(product["Quantity"])
+    total_price= price*quantity
+    new_data.append([name,price,quantity,total_price])
+
+
+new_file_path=os.path.join(script_dir,"new_products.csv")
+with open(new_file_path,"w",newline='') as file:
+    writer=csv.writer(file)
+    writer.writerow(["Product Name","Price","Quantity","Total Price"])
+    writer.writerows(new_data)
